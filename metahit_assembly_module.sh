7f95d4ea2fc6bbf34db3c474f33185f6a1e193fb
Aldeen5678 <105819369+Aldeen5678@users.noreply.github.com>
2026-06-12 00:54:30 -0500

    Update assembly_module.sh

--- metahit_assembly_module.sh
+++ metahit_assembly_module.sh
@@ -1,22 +1,9 @@
-#!/bin/bash
-#SBATCH --partition=compute1          
-#SBATCH --ntasks=1                    
-#SBATCH --nodes=1                     
-#SBATCH --cpus-per-task=80             
-#SBATCH --time=24:00:00             
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=aaron.gonzalez@utsa.edu
-
-module load anaconda3
-conda activate metahit_env
-
-# Assign parameters
-
 METAHIT_PROJECT_PATH=${1}
 OUT=${2}
 FORWARD=${3}
 REVERSE=${4}
 ASSEMBLER=${5}
+
 
 cd ${METAHIT_PROJECT_PATH}
 
