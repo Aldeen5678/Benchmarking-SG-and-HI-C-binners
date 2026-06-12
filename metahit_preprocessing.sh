a652f7c49a149db938da4fd2ea8ad7a1635f9868
Aldeen5678 <105819369+Aldeen5678@users.noreply.github.com>
2026-06-12 00:55:02 -0500

    Update metahit_preprocessing.sh

--- metahit_preprocessing.sh
+++ metahit_preprocessing.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-#SBATCH --partition=compute1          
-#SBATCH --ntasks=1                    
-#SBATCH --nodes=1                     
-#SBATCH --cpus-per-task=80             
-#SBATCH --time=24:00:00             
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=aaron.gonzalez@utsa.edu
-
-module load anaconda3
-conda activate metahit_env
-
-# Assign parameters
-
 METAHIT_PROJECT_PATH=${1}
 OUT=${2}
 FORWARD=${3}
