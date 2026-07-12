8cf29af00b031a16028f5e12440a21e1049d1646
Aldeen5678 <105819369+Aldeen5678@users.noreply.github.com>
2026-07-12 00:16:20 -0500

    Refactor variable names for bin refinement paths

--- metawrap_module.sh
+++ metawrap_module.sh
@@ -1,18 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=ww_megahit_metawrap       # Job name
-#SBATCH --partition=compute1          # Partition
-#SBATCH --ntasks=1                    # Number of tasks (processes, always 1 for non-MPI jobs)
-#SBATCH --nodes=1                     # Numner of nodes (Alway 1 for non-MPI jobs)
-#SBATCH --cpus-per-task=80             # Cores per task
-#SBATCH --time=10:00:00               # Time limit (hh:mm:ss)
-#SBATCH --output=./logs/ww_megahit_metawrap.log        # Standard output file, or system will create a output file if output is not specified.
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=aaron.gonzalez@utsa.edu #Job status (starting, finishing, etc) will be sent to this email address. 
-
-module load anaconda3
-conda activate metahit_env
-
-
 METAHIT_PROJECT_PATH=${1}
 bin_refinement_path=${METAHIT_PROJECT_PATH}/modules/6_binning/scripts
 OUT=${2}
@@ -30,12 +15,12 @@ TIME_CMD="/usr/bin/time -a -o $RESOURCE_LOG --format='Command: %C\nElapsed Time:
 
 echo "Running metawrap refinment..."
 
-COMEBIN_BIN_PATH=${3}
-IMPUTECC_BIN_PATH=${4}
-METACC_BIN_PATH=${5}
+BINNER1_BIN_PATH=${3}
+BINNER2_BIN_PATH=${4}
+BINNER3_BIN_PATH=${5}
 
 
-BASH_CMD="sh ${bin_refinement_path}/bin_refinement.sh -o ./ -t 80 -A ${COMEBIN_BIN_PATH} -B ${IMPUTECC_BIN_PATH} -C ${METACC_BIN_PATH}"
+BASH_CMD="sh ${bin_refinement_path}/bin_refinement.sh -o ./ -t 80 -A ${BINNER1_BIN_PATH} -B ${BINNER2_BIN_PATH} -C ${BINNER3_BIN_PATH}"
 echo "Executing the following command: ${BASH_CMD}"
 
 export LC_ALL=C
