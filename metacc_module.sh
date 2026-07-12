3246bfce78e2438f6773f8f0fa74603dca57eaef
Aldeen5678 <105819369+Aldeen5678@users.noreply.github.com>
2026-07-12 17:20:06 -0500

    Remove timer and resource reporting from metacc_module.sh
    Removed timer and resource reporting section from script.

--- metacc_module.sh
+++ metacc_module.sh
@@ -5,10 +5,6 @@ BAM_FILE=${4}
 DASTOOL_PATH=${5}
 ENZYME=""
 
-# Start timer for internal logging
-START_TIME=$(date +%s)
-
-
 echo "Running METADECODER binning algorithm..."
 RESULTS_DIR="${OUTPUT_DIR}/metacc_results"
 
@@ -23,16 +19,7 @@ echo "Executing command: ${BASH_CMD}"
 eval $BASH_CMD
 
 # --- RESOURCE REPORTING SECTION ---
-END_TIME=$(date +%s)
-DURATION=$((END_TIME - START_TIME))
-echo "------------------------------------------------"
-echo "JOB RESOURCE STATISTICS"
-echo "------------------------------------------------"
-echo "Script-calculated Duration: $((DURATION / 60)) minutes"
-sleep 20
-echo "Slurm Official Metrics for Job $SLURM_JOB_ID:"
-sacct -j $SLURM_JOB_ID --format=JobID,JobName,Elapsed,MaxRSS,State
-echo "------------------------------------------------"
+-----------------------------------------"
 
 echo "Creating contig-to-bin files"
 conda deactivate
@@ -40,4 +27,3 @@ conda activate das_tool
 ${DASTOOL_PATH}/src/Fasta_to_Contig2Bin.sh -e fa -i ${RESULTS_DIR}/BIN > ${RESULTS_DIR}/contig_bins.tsv
 
 
-checkm2 predict --threads 80 --input ${RESULTS_DIR}/BIN --output-directory ${RESULTS_DIR}/checkm2 -x .fa
